ad0ccc648c85438221d615beed98acb42bcb347a
zenway33m <zenway33@gmail.com>
2017-09-28 10:04:01 +0800

    example update

--- codelib_example.py
+++ codelib_example.py
@@ -7,3 +7,16 @@ with open('somefile.txt', 'a') as f:
 
 with open("/tmp/foo.txt") as f:
     data = f.read()
+
+with open('a.txt') as f:
+	for i in f:
+	    print(i)
+	print(f.read())        # 打印所有内容为字符串
+	print(f.readlines())   # 打印所有内容按行分割的列表
+
+# 如何输出漂亮的json格式
+from pprint import pprint
+i = r.json
+pprint(i)  即可格式漂亮的格式
+
+
